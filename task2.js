ff675e3cbd158b7f9d5d84da7ca73f03801343eb
amin515 <nil.somudra51@gmail.com>
2021-12-12 21:55:42 +0600

    add new file

--- task2.js
+++ task2.js
@@ -164,9 +164,165 @@
       }]
 
     }
-  ]
+    
+  ];
 
-  let data = JSON.stringify(student);
-  console.log(data)
+  const family = [
+    {
+      name      : "Shahriar Somrat",
+      rel       : "Friends",
+      phone     : "0178546532",
+      location  : "Bogura"
+    },
+    
+    {
+      name      : "Abu Achem",
+      rel       : "Friends",
+      phone     : "0113252464",
+      location  : "Bogura"
+    },
+    {
+      name      : "Nur Amin",
+      rel       : "Friends",
+      phone     : "01303292586",
+      location  : "Bogura"
+    },  {
+      name      : "Sabbir Rahman",
+      rel       : "Friends",
+      phone     : "017875463",
+      location  : "Chittagong"
+    },  {
+      name      : "Sumaya Tasneem",
+      rel       : "Friends",
+      phone     : "0170011258",
+      location  : "Bogura"
+    },  {
+      name      : "Abu Raihan",
+      rel       : "Friends",
+      phone     : "0125488654",
+      location  : "Netrokona"
+    },  {
+      name      : "Humayun Kabir",
+      rel       : "Friends",
+      phone     : "011524876",
+      location  : "Sylhet"
+    },  {
+      name      : "Mamun Islam",
+      rel       : "Friends",
+      phone     : "0178543236",
+      location  : "Rangpur"
+    },  {
+      name      : "Abu Sufian",
+      rel       : "Friends",
+      phone     : "017526485",
+      location  : "Dhaka"
+    },  {
+      name      : "Abu Sayeed",
+      rel       : "Friends",
+      phone     : "+99-0214587",
+      location  : "Cumilla"
+    },
 
-  localStorage.setItem(data)
+  ];
+
+  const devs = [
+    {
+ 
+      id       : 1,
+      name     : 'Nur Amin',
+      age      : 32,
+      skill    : 'MERN Stack',
+      location : 'Bogura'
+
+    },
+    {
+ 
+      id       : 1,
+      name     : 'Shariar Somarat',
+      age      : 32,
+      skill    : 'IOS',
+      location : 'Bogura'
+
+    },
+    {
+ 
+      id       : 3,
+      name     : 'Sabbir Rahman',
+      age      : 32,
+      skill    : 'Linux',
+      location : 'Rangpur'
+
+    },
+    {
+ 
+      id       : 4,
+      name     : 'Sumaya Tasneem',
+      age      : 42,
+      skill    : 'Node.js',
+      location : 'Chuadanga'
+
+    },
+    {
+ 
+      id       : 5,
+      name     : 'Humayun Kabir',
+      age      : 32,
+      skill    : 'mongoDB',
+      location : 'Tangail'
+
+    },
+    {
+ 
+      id       : 6,
+      name     : 'Abu Sayeed',
+      age      : 29,
+      skill    : 'React.js',
+      location : 'Cumilla'
+
+    },
+    {
+ 
+      id       : 7,
+      name     : 'Abu Achem',
+      age      : 37,
+      skill    : 'JavaScript',
+      location : 'Dhaka'
+
+    },{
+ 
+      id       : 8,
+      name     : 'Abu Raihan',
+      age      : 34,
+      skill    : 'Django',
+      location : 'Sherpur'
+
+    },
+    {
+ 
+      id       : 9,
+      name     : 'Mamun Islam',
+      age      : 31,
+      skill    : 'Java',
+      location : 'Rankgpur'
+
+    },
+    {
+ 
+      id       : 10,
+      name     : 'Abu Sufian',
+      age      : 35,
+      skill    : 'Python',
+      location : 'Cumilla'
+
+    }
+    
+  ];
+
+
+
+ let data = JSON.stringify(family);
+ let data1 = JSON.stringify(devs);
+ let data2 = JSON.stringify(student)
+ 
+console.log(data1);
+
